c8b655415c337154f28aa5a39dae71d8172b83a5
jbwp-eu <jarekb0605@gmail.com>
2026-03-05 15:57:51 +0100

    update StoreLocation.tsx

--- frontend/src/components/StoreLocator.tsx
+++ frontend/src/components/StoreLocator.tsx
@@ -39,7 +39,8 @@ const StoreLocator = () => {
           <AlertDialogDescription>
             <div className="mt-4">
               <iframe
-                src="https://sl-widget.proguscommerce.com/main?shop=96aba7ff-5ddf-49cf-8299-de535c9bb02b306"
+                // src="https://sl-widget.proguscommerce.com/main?shop=96aba7ff-5ddf-49cf-8299-de535c9bb02b306"
+                src="https://sl-widget.proguscommerce.com/main?shop=f2e861df-1afc-44be-ad9f-ee22d1779fa3313"
                 width="100%"
                 height="500"
                 style={{ border: 0 }}
